2315fa7f8c7615a12b443f2161149e5df140c889
Nikola Jeric <jergic.nikola358@gmail.com>
2022-03-18 22:02:06 +0100

    create-post

--- create-post.php
+++ create-post.php
@@ -1,3 +1,21 @@
-<?php include 'footer.php';?>
-<?php include 'header.php';?>
-<?php include 'side.bar';?>
+<?php include 'db.php'?>
+<?php
+ini_set("display_errors",1);
+
+$sql = "INSERT INTO posts (title,body,author) VALUES ('{$_POST['title']}','{$_POST['body']}','{$_POST['author']}')";
+    
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+
+?>
+<form action="create-post.php" method="post">
+  <label for="title">Title:</label><br>
+  <input type="text" id="title" name="title" value=""><br><br>
+  <label for="body">Body:</label><br>
+  <textarea id ='body' name='body' rows='5' cols='13'></textarea><br><br>
+    <label for='author'>Author:</label><br>
+  <input type="text" id='author' name='author' value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+  
+</form> 
