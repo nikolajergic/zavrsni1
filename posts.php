d8d2dcc091951d59342f95fd5d9c3a74abadb069
Nikola Jeric <jergic.nikola358@gmail.com>
2022-03-18 20:20:08 +0100

    Peti zadatak

--- posts.php
+++ posts.php
@@ -2,12 +2,13 @@
 <?php
     $sql = "SELECT * FROM posts ORDER BY created_at DESC" ;
     $posts = getDataFromServer($sql, $connection, true);
+    
 ?>
 
 <div class="col-sm-8 blog-main">
     <?php foreach ($posts as $post) { ?>
         <div class="blog-post">
-            <a href="" class="blog-post-title">
+            <a href="single-post.php?id=<?php echo $post['id']?>" class="blog-post-title">
                 <h2><?php echo $post['title'] ?></h2>
             </a>
             <p class="blog-post-meta"><?php echo date_format(date_create($post['created_at']), 'd-F-Y') ?> by <a href="#"><?php echo $post['author'] ?></a></p>
--- single-post.php
+++ single-post.php
@@ -1,3 +1,59 @@
-<?php include 'footer.php';?>
+<?php
+ini_set('display_errors', 1);
+?>
+<!-- <?php include 'footer.php';?>
 <?php include 'header.php';?>
-<?php include 'side.bar';?>
+<?php include 'side.bar';?> -->
+
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link href="styles/blog.css" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+<?php include 'db.php';?>
+<?php
+    $post_id = $_GET['id'];
+    if (empty($post_id)) {
+        header("Location: http://localhost:8000/index.php");
+    }
+    $sql = "SELECT * FROM posts WHERE id = $post_id";
+    $post = getDataFromServer($sql, $connection, false);
+    
+?>
+
+ <?php include_once('db.php')?>
+<?php
+    
+    $sql = "SELECT * FROM comments WHERE post_id = $post_id";
+    $comments = getDataFromServer($sql, $connection, true);
+?>
+
+<?php include('header.php') ?>
+<div class="col-sm-8 blog-main">
+    
+        <div class="blog-post">
+            <a href="">
+                <h2  class="blog-post-title"><?php echo $post['title'] ?></h2>
+            </a>
+            <p class="blog-post-meta"><?php echo date_format(date_create($post['created_at']), 'd-F-Y') ?> by <a href="#"><?php echo $post['author'] ?></a></p>
+            <p><?php echo $post['body'] ?></p>
+    </div>
+  
+    <nav class="blog-pagination">
+        <a class="btn btn-outline-primary" href="#">Older</a>
+        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+    </nav>
+    <h2  class="blog-post-title">Comments: </h2>
+    <ul class='list-control'>
+                        <?php foreach ($comments as $comment) { ?>
+                            <li class='comment-item'>
+                                <h4><?php echo $comment['author'] ?></h4>
+                                <p><?php echo $comment['text'] ?></p>
+                                <hr>
+                            </li>
+                        <?php } ?>
+                    </ul>
+
+     <?php include('footer.php') ?>
+
+</div>
